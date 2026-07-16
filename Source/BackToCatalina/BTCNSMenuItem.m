0a1f75d9f2cadfa1834581e9ff451bac17f34ca9
Ittr <79785147+ittrgrey@users.noreply.github.com>
2026-07-16 13:11:37 +0100

    Old menu indentation behaviour (i think?)

---
+++ Source/BackToCatalina/BTCNSMenuItem.m
@@ -0,0 +1,16 @@
+//
+//  BTCNSMenuItem.m
+//  BackToCatalina
+//
+//  Created by ittrgrey on 15/07/2026.
+//
+
+#include <Cocoa/Cocoa.h>
+#include "ZKSwizzle.h"
+
+hook(NSMenuItem)
+
+- (NSInteger)indentationLevel {
+    return MAX(1, ZKOrig(NSInteger));
+}
+endhook
