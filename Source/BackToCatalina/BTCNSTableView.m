ab3223e75709778bf6eeaa50738dec69b8fc9e7d
Ittr <79785147+ittrgrey@users.noreply.github.com>
2026-07-16 13:09:58 +0100

    Make NSSidebarUsesGoldenMetrics enabled state look marginally less ugly
    Background inset eliminated, corner radius eliminated. Needs a lot of further tweaks before use of this mode can be recommended.

--- Source/BackToCatalina/BTCNSTableView.m
+++ Source/BackToCatalina/BTCNSTableView.m
@@ -6,3 +6,18 @@ hook(NSTableView)
     return 2;
 }
 endhook
+
+hook(NSTableViewStyleData)
+
+// If NSSidebarUsesGoldenMetrics are on, it results in stuff being rounded and looking strange
+// This addresses that - other differentials do however remain at the moment.
+
+- (double)rowBackgroundInset {
+    return 0;
+}
+
+- (double)cornerRadius {
+    return 0;
+}
+
+endhook
