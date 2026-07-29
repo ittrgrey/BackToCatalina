97f2bc8d907c4564f8a20b732295b37c02c9cf12
Ittr <79785147+ittrgrey@users.noreply.github.com>
2026-07-29 14:55:52 +0100

    Forcefully disable Solarium by feature flag
    This is an additional fallback, but it also allows for Solarium to be enabled developer-side adhoc without needing to reboot macOS. This may prove useful in future years when Solarium is (eventually) forced, or system components start to break with it disabled.

--- Source/BackToCatalina/BackToCatalina.m
+++ Source/BackToCatalina/BackToCatalina.m
@@ -6,6 +6,20 @@
 NSBundle* carBundle;
 BOOL isTahoeOrLater;
 
+// Disable Solarium by fusing it to be disabled..
+// ..unless we are ControlCenter or NotificationCenterUI
+Boolean (*_os_feature_enabled_impl)(const char* domain, const char* feature);
+Boolean BTC_os_feature_enabled_impl(const char* domain, const char* feature) {
+    Boolean result = _os_feature_enabled_impl(domain, feature);
+    if (domain && feature) {
+         if (strcmp(domain, "SwiftUI") == 0 && strcmp(feature, "Solarium") == 0) {
+             return ([[[NSBundle mainBundle] bundleIdentifier] isEqualToString:@"com.apple.notificationcenterui"] || [[[NSBundle mainBundle] bundleIdentifier] isEqualToString:@"com.apple.controlcenter"]) ? true : false;
+         }
+    }
+    
+    return result;
+}
+
 Boolean (*CompatWidgetOld)(void);
 Boolean CompatWidgetNew(void) {
     return true;
@@ -31,8 +45,14 @@ WEAK_IMPORT_ATTRIBUTE
     // This loads from a bundle that contains the asset files, but otherwise has been renamed etc so that it isn't wiped during system updates
     carBundle = [NSBundle bundleWithPath:@"/private/var/ammonia/core/tweaks/libBackToCatalina/BTC_VisualStyle.bundle"];
     
+    // Check if we are on Tahoe or later
     isTahoeOrLater = [NSProcessInfo.processInfo isOperatingSystemAtLeastVersion:tahoeVersion];
-
+    
+    // Disable Solarium by hooking an exported function system-wide, as a fallback and additional layer to ensure it is disabled
+    DobbyHook(DobbySymbolResolver(NULL, "_os_feature_enabled_impl"),
+              BTC_os_feature_enabled_impl,
+                  &_os_feature_enabled_impl);
+    
     DobbyHook(DobbySymbolResolver("AppKit", "_NSToolbarItemViewerCompatabilitySelectionWidgetDefaultValueFunction"),
               CompatWidgetNew,
               &CompatWidgetOld);
