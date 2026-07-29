c6fdf3a665b1fc2bb5fc92cf1e8bf8821618dcab
Ittr <79785147+ittrgrey@users.noreply.github.com>
2026-07-29 13:02:28 +0100

    Fix control sizes for NSSegmented controls

--- Source/BackToCatalina/BTCNSSegmentItemView.m
+++ Source/BackToCatalina/BTCNSSegmentItemView.m
@@ -11,6 +11,13 @@ hook(NSSegmentItemView)
     return NO;
 }
 
+- (void)setControlSize:(NSControlSize)controlSize {
+   // NSControlSizeLarge did not exist prior to macOS 11
+   if (controlSize == NSControlSizeLarge) controlSize = NSControlSizeRegular;
+   
+   return ZKOrig(void, controlSize);
+}
+
 endhook
 
 
@@ -28,3 +35,15 @@ hook(NSSegmentedCell)
 }
 
 endhook
+
+hook(NSSegmentedControlBezelConfiguration)
+
+- (void)setControlSize:(NSControlSize)controlSize {
+   // NSControlSizeLarge did not exist prior to macOS 11
+   if (controlSize == NSControlSizeLarge) controlSize = NSControlSizeRegular;
+   
+   return ZKOrig(void, controlSize);
+}
+
+endhook
+
