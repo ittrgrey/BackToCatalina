fd360b546ec9a2dcb30a46cea131789203a00d0d
Ittr <79785147+ittrgrey@users.noreply.github.com>
2026-07-29 13:02:37 +0100

    Revert NSTabView sizing

---
+++ Source/BackToCatalina/BTCNSTabView.m
@@ -0,0 +1,20 @@
+//
+//  BTCNSTabView.m
+//  BackToCatalina
+//
+//  Created by ittrgrey on 29/07/2026.
+//
+
+#include <Cocoa/Cocoa.h>
+#include "ZKSwizzle.h"
+
+hook(NSTabView)
+
+- (void)setControlSize:(NSControlSize)controlSize {
+    // NSControlSizeLarge did not exist prior to macOS 11
+    if (controlSize == NSControlSizeLarge) controlSize = NSControlSizeRegular;
+    
+    return ZKOrig(void, controlSize);
+}
+
+endhook
