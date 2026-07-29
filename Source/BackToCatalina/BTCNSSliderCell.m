08cc4aa336179a731039f033f3b79e03119ae1b3
Ittr <79785147+ittrgrey@users.noreply.github.com>
2026-07-29 13:02:57 +0100

    Revert NSSliderCell sizing

--- Source/BackToCatalina/BTCNSSliderCell.m
+++ Source/BackToCatalina/BTCNSSliderCell.m
@@ -6,4 +6,12 @@ hook(NSSliderCell)
     // Slider in Music app
     return NO;
 }
+
+- (void)setControlSize:(NSControlSize)controlSize {
+    // NSControlSizeLarge did not exist prior to macOS 11
+    if (controlSize == NSControlSizeLarge) controlSize = NSControlSizeRegular;
+    
+    return ZKOrig(void, controlSize);
+}
+
 endhook
