4af2f1d36ee94657daca6c3b6e9b56ff876e8127
Ittr <79785147+ittrgrey@users.noreply.github.com>
2026-08-10 12:49:36 +0100

    Disable modern separator in most places
    Currently still appears when viewing the Bin in Finder. Investigating how to fix that currently

--- Source/BackToCatalina/BTCNSWindow.m
+++ Source/BackToCatalina/BTCNSWindow.m
@@ -56,6 +56,16 @@ hook(NSWindow)
     return [NSBundle.mainBundle.bundleIdentifier isEqualToString:@"com.apple.systempreferences"] ? 1 : ZKOrig(long long);
 }
 
+// Get rid of the new translucent separator for standard titlebars
+- (NSTitlebarSeparatorStyle)titlebarSeparatorStyle {
+    return NSTitlebarSeparatorStyleNone;
+}
+
+// Get rid of the new translucent separator for toolbar titlebars
+- (NSTitlebarSeparatorStyle)contentTitlebarSeparatorStyle {
+    return NSTitlebarSeparatorStyleNone;
+}
+
 endhook
 
 hook(NSSheetMoveHelper)
